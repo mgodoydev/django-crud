79f9c5ba75719171dca419ef7e66015e77f87cf5
Mathias Godoy <mathiasgodoydev@gmail.com>
2024-09-08 23:13:00 -0400

    Solucionando problemas del deploy

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
+        default='postgresql://postgres:PostgreSQL@localhost/postgres',
         conn_max_age=600
     )
 }
